8add6392e8ef835000dcccc08bc459af403dc666
Tino Mutano <tino.mutano@hotmail.com>
2021-11-28 16:22:16 +0100

    Added comments

--- BubbleSort/Tester.java
+++ BubbleSort/Tester.java
@@ -1,34 +1,45 @@
+import java.util.Random;
+
 import operations.Operations;
 
 public class Tester {
     public static void main(String[] args) {
         int nItems = 10;
-        Operations arr;
-        arr = new Operations(nItems);
+        Operations ops;
+        Random rnum = new Random();
+        ops = new Operations(nItems); // creating Operations object
 
-        arr.insert(0, 1);
-        arr.insert(1, 8);
-        arr.insert(2, 4);
-        arr.insert(3, 6);
-        arr.insert(4, 0);
-        arr.insert(5, 3);
-        arr.insert(6, 5);
-        arr.insert(7, 2);
-        arr.insert(8, 7);
-        arr.insert(9, 9);
+        // using insert method to add values to array
+/*         ops.insert(0, 1);
+        ops.insert(1, 8);
+        ops.insert(2, 4);
+        ops.insert(3, 6);
+        ops.insert(4, 0);
+        ops.insert(5, 3);
+        ops.insert(6, 5);
+        ops.insert(7, 2);
+        ops.insert(8, 7);
+        ops.insert(9, 9); */
+
+        for(int i = 0; i < nItems; i++){
+            ops.insert(i, rnum.nextInt(50));
+        }
+
+
 
         System.out.print("Pre-sort array: ");
-        arr.display();
+        ops.display(); // Displaying array before sorting
         System.out.println("");
 
 
         System.out.println("");
-        arr.bubbleSort();
+        ops.bubbleSort(); // Calling bubbleSort algo
+    
         System.out.println("");
 
         System.out.print ("Post-sort array: ");
         System.out.println("");
-        arr.display();
+        ops.display(); // Displaying array after sorting
 
     }
 
--- BubbleSort/operations/Operations.java
+++ BubbleSort/operations/Operations.java
@@ -2,46 +2,50 @@ package operations;
 
 
 public class Operations{
-    int[] arr;
+    int[] arr; // declaring array
     int temp = 0;
     int i = 0;
     int j = 0;
 
 
    public Operations(int init){
-       arr = new int[init];
+       arr = new int[init]; //initializing array with parameter 'init' value(s) 
    }
 
    public void insert(int index, int value){
-        arr[index] = value;
+        arr[index] = value; //overwriting value at index position
    }
 
    public void delete(int index){
-        for(i = 0; i < arr.length-1; i++){
-            arr[index] = arr[index + 1];
+        for(i = 0; i < arr.length-1; i++){ // arr.length-1 = last index position
+            arr[index] = arr[index + 1]; // Assigning value adjacent to arr[index] to arr[index]
         } 
      }
 
     public void find(int value){
     for(i = 0; i < arr.length; i++){
-        if(arr[i] == value){
+        if(arr[i] == value){ // check to see if int value at arr[i] is the same as the int value parameter
             System.out.println("\nValue: " + value + " found at index pos " + i);
-            break;
+            break; // break loop if value is found in the array
         }
-    } if(i == arr.length){
+    } if(i == arr.length){ // if true, then we looped through the whole thing and didn't find the value in the array
             System.out.println("\nValue not found in the Array");
         } 
     }
 
     public void bubbleSort(){
+
         boolean swapped = false;
+        long start = System.nanoTime();
 
         for(i = 0 ; i < arr.length; i++ ){
             swapped = false;
-            for(j = 0; j < (arr.length-1-i); j++){
-                
-                System.out.printf("Items compared: [%d, %d]",arr[j],arr[j+1]);
 
+            for(j = 0; j < (arr.length-1-i); j++){ // (arr.length-1-i) == 9 at first iteration, reducing by i(1) each time.
+                                                    // essentially shortening the array after each iteration because the
+                                                    // largest value is pushed to the end of the array each time.
+                System.out.printf("Items compared: [%d, %d]",arr[j],arr[j+1]);
+                // Swapping lower values with higher ones, shifting to the right.
                 if(arr[j] > arr[j+1]){
                     temp = arr[j];
                     arr[j] = arr[j+1];
@@ -55,8 +59,9 @@ public class Operations{
                     System.out.print(" => not swapped\n");
                 }
 
-            }if(!swapped){
-                break;
+            }
+            if(!swapped){ // this means that if swapped is not equal to true
+                break; // nothing was swapped, so we assume the array has been sorted and break out
             }
             System.out.println("");
             System.out.printf("Iteration %d#: ", (i+1));
@@ -64,9 +69,14 @@ public class Operations{
             System.out.println("");
             System.out.println("");
 
-
         }
-
+        // 
+        long end = System.nanoTime();
+        long execNanoSeconds = end - start;
+        double execTimeSeconds = (double)execNanoSeconds / 1_000_000_000;
+        double finalTime = Math.round(execTimeSeconds * 100.0) / 100.0;
+        System.out.println("");
+        System.out.println("Execution time: " + finalTime + " seconds ~");
     }
     
 
